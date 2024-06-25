146dfc8f6ce6cce92dcf77b9d6f6d9f557e6f883
A91y <65825207+A91y@users.noreply.github.com>
2024-06-26 02:45:07 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::Command;
 
 const BUILT_IN_COMMANDS: [&str; 3] = ["echo", "exit", "type"];
 fn main() {
@@ -24,7 +25,10 @@ fn main() {
                 handle_type(&path_env, &tokens[1].trim());
             }
             _ => {
-                println!("{}: command not found", tokens[0].trim());
+                if tokens.len() == 1 && tokens[0].trim() == "" {
+                    continue;
+                }
+                handle_command(&path_env, &tokens[0..].join(" "));
             }
         }
         input.clear()
@@ -45,4 +49,24 @@ fn handle_type(path: &str, cmd: &str) {
         }
         println!("{}: not found", cmd);
     }
+}
+
+fn handle_command(path: &str, cmd: &str) {
+    let dirs: Vec<&str> = path.split(":").collect();
+    for dir in dirs {
+        let command = cmd.split(" ").collect::<Vec<&str>>()[0];
+        let arg = cmd.split(" ").collect::<Vec<&str>>()[1..].join(" ");
+        let path = format!("{}/{}", dir, command);
+        if std::path::Path::new(&path).exists() {
+            let output;
+            if arg == "" {
+                output = Command::new(path).output().unwrap();
+            } else {
+                output = Command::new(path).arg(arg).output().unwrap();
+            }
+            println!("{}", String::from_utf8_lossy(&output.stdout).trim());
+            return;
+        }
+    }
+    println!("{}: command not found", cmd);
 }
